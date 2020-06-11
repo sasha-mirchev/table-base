3de6af57b881af2161cd69671568c22f4d0944c5
Александр <sasha.mirchev@gmail.com>
2020-06-11 00:19:35 -0700

    Updates

---
+++ precache-manifest.36926912f3fe892fc31e5f538105244c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271495c528b9737b08752f7de0e31d03",
+    "url": "/table-base/index.html"
+  },
+  {
+    "revision": "7b73788b951860dabc66",
+    "url": "/table-base/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0d72c3cba6a37ce83b15",
+    "url": "/table-base/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "7b73788b951860dabc66",
+    "url": "/table-base/static/js/2.0085bc5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/table-base/static/js/2.0085bc5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d72c3cba6a37ce83b15",
+    "url": "/table-base/static/js/main.eb9cbdbc.chunk.js"
+  },
+  {
+    "revision": "564c1269940ed52b95a9",
+    "url": "/table-base/static/js/runtime-main.033af7c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/table-base/static/media/logo.5d5d9eef.svg"
+  }
+]);
